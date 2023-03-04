f40136ab988fa92a39bb79896f4e5e7ed833c423
christopheilermann <user@purebase.de>
2023-03-04 11:43:07 +0100

    Adding experimental code ...

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # 2023.03--spotify-by-nextjs--stackblitz
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-ys9kjg)
+
+https://github.com/purebase/2023.03--spotify-by-nextjs--stackblitz
+
+
+# How to connect spotify web api for NextJS?
+
+By Lib:
+https://github.com/thelinmichael/spotify-web-api-node
+
+By Hand:
+https://dev.to/j471n/how-to-use-spotify-api-with-nextjs-50o5
+
+Netlify?:
+https://spacejelly.dev/posts/how-to-authenticate-spotify-web-api-requests-in-next-js-with-netlify-api-auth/
--- pages/api/connect.js
+++ pages/api/connect.js
@@ -1,4 +1,29 @@
-const spotifyApi = new SpotifyWebApi({
-  clientId: '<insert client id>',
-  clientSecret: '<insert client secret>',
-});
+import SpotifyWebApi from "spotify-web-api-node";
+
+export default (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+
+  spotifyApi
+      .authorizationCodeGrant(code)
+      .then(data => {
+        res.json({
+          accessToken: data.body.access_token,
+          refreshToken: data.body.refresh_token,
+          expiresIn: data.body.expires_in,
+        })
+      })
+      .catch(err => {
+        res.sendStatus(400)
+      })
+
+
+
+
+
+  res.status(200).json({ name: 'Hello, world!' });
+};
